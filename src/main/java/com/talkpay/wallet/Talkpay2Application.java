3e23d11f9376b0344034c6aeeecf2336727d8f31
KadaliThanojKumar <2300032859@kluniversity.in>
2025-09-23 20:07:40 +0530

    Add built WAR and backend updates

--- src/main/java/com/talkpay/wallet/Talkpay2Application.java
+++ src/main/java/com/talkpay/wallet/Talkpay2Application.java
@@ -11,6 +11,27 @@
 //	}
 //
 //}
+//
+//package com.talkpay.wallet;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//@SpringBootApplication
+//public class Talkpay2Application extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Talkpay2Application.class);
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Talkpay2Application.class, args);
+//    }
+//}
+
 
 package com.talkpay.wallet;
 
