3f3acd6ad75c9616e4317d1e08147587b05b1535
KadaliThanojKumar <2300032859@kluniversity.in>
2025-09-23 19:52:28 +0530

    Add built jar file

--- src/main/java/com/talkpay/wallet/Talkpay2Application.java
+++ src/main/java/com/talkpay/wallet/Talkpay2Application.java
@@ -1,13 +1,33 @@
+//package com.talkpay.wallet;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class Talkpay2Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(Talkpay2Application.class, args);
+//	}
+//
+//}
+
 package com.talkpay.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Talkpay2Application {
+public class Talkpay2Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Talkpay2Application.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Talkpay2Application.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Talkpay2Application.class, args);
+    }
 }
