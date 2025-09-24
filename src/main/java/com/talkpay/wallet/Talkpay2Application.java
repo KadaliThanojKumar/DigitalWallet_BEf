8ef7138f5eb66501e554782eb1100bed18a19790
KadaliThanojKumar <2300032859@kluniversity.in>
2025-09-24 08:29:13 +0530

    first commit

--- src/main/java/com/talkpay/wallet/Talkpay2Application.java
+++ src/main/java/com/talkpay/wallet/Talkpay2Application.java
@@ -32,6 +32,46 @@
 //    }
 //}
 
+//
+//package com.talkpay.wallet;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//@SpringBootApplication
+//public class Talkpay2Application extends SpringBootServletInitializer {
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Talkpay2Application.class);
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Talkpay2Application.class, args);
+//    }
+//}
+//
+//
+
+//package com.talkpay.wallet;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//@SpringBootApplication
+//public class Talkpay2Application extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(Talkpay2Application.class);
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Talkpay2Application.class, args);
+//    }
+//}
 
 package com.talkpay.wallet;
 
@@ -39,16 +79,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 public class Talkpay2Application extends SpringBootServletInitializer {
+
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Talkpay2Application.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Talkpay2Application.class);
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Talkpay2Application.class, args);
     }
 }
-
-
